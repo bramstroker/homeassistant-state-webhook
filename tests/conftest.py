99d4d42756ee44d0c5438df97f993304be796b7e
Bram <bgerritsen@emico.nl>
2024-12-18 16:52:00 +0100

    feat: add happyflow test

---
+++ tests/conftest.py
@@ -0,0 +1,59 @@
+import asyncio
+from collections.abc import Generator
+
+import pytest
+from homeassistant import loader
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.area_registry import AreaRegistry
+from homeassistant.helpers.device_registry import DeviceRegistry
+from homeassistant.helpers.entity_registry import EntityRegistry
+from pytest_homeassistant_custom_component.common import (
+    mock_device_registry,
+    mock_registry,
+)
+
+
+@pytest.fixture(autouse=True)
+def auto_enable_custom_integrations(enable_custom_integrations: bool) -> Generator:
+    yield
+
+
+@pytest.fixture(autouse=True)
+def enable_event_loop_debug(event_loop: asyncio.AbstractEventLoop) -> None:
+    """Override the fixture to disable event loop debug mode."""
+    event_loop.set_debug(False)  # Modify this behavior as needed
+
+
+@pytest.fixture(autouse=True)
+def expected_lingering_timers() -> bool:
+    """Temporary ability to bypass test failures.
+    Parametrize to True to bypass the pytest failure.
+    @pytest.mark.parametrize("expected_lingering_timers", [True])
+    This should be removed when all lingering timers have been cleaned up.
+    See https://github.com/MatthewFlamm/pytest-homeassistant-custom-component/issues/153
+    """
+    return True
+
+
+@pytest.fixture
+def enable_custom_integrations(hass: HomeAssistant) -> None:
+    """Enable custom integrations defined in the test dir."""
+    hass.data.pop(loader.DATA_CUSTOM_COMPONENTS)
+
+
+@pytest.fixture
+def area_reg(hass: HomeAssistant) -> AreaRegistry:
+    """Return an empty, loaded, registry."""
+    return mock_area_registry(hass)
+
+
+@pytest.fixture
+def device_reg(hass: HomeAssistant) -> DeviceRegistry:
+    """Return an empty, loaded, registry."""
+    return mock_device_registry(hass)
+
+
+@pytest.fixture
+def entity_reg(hass: HomeAssistant) -> EntityRegistry:
+    """Return an empty, loaded, registry."""
+    return mock_registry(hass)
---
+++ tests/test_init.py
@@ -0,0 +1,54 @@
+from aioresponses import aioresponses
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_registry import RegistryEntry
+from pytest_homeassistant_custom_component.common import MockConfigEntry, mock_registry
+
+from custom_components.state_webhook import CONF_ENTITY_DOMAIN, CONF_WEBHOOK_URL, DOMAIN, async_setup_entry
+
+
+DEFAULT_WEBHOOK_URL = "https://example.com/webhook"
+
+async def test_state_webhook_triggered_successfully(hass: HomeAssistant) -> None:
+    mock_registry(
+        hass,
+        {
+            "input_boolean.test": RegistryEntry(
+                entity_id="input_boolean.test",
+                name="Test",
+                unique_id="test",
+                platform="input_boolean",
+            ),
+            "input_boolean.test2": RegistryEntry(
+                entity_id="input_boolean.test2",
+                name="Test2",
+                unique_id="test2",
+                platform="input_boolean",
+            ),
+        },
+    )
+    hass.states.async_set("input_boolean.test", "on")
+    hass.states.async_set("input_boolean.test2", "off")
+    await hass.async_block_till_done()
+
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        options={
+            CONF_WEBHOOK_URL: DEFAULT_WEBHOOK_URL,
+            CONF_ENTITY_DOMAIN: "input_boolean",
+        }
+    )
+
+    await async_setup_entry(hass, entry)
+
+    with aioresponses() as http_mock:
+        http_mock.post(DEFAULT_WEBHOOK_URL, status=200)
+
+        hass.states.async_set("input_boolean.test", "off")
+        await hass.async_block_till_done()
+
+        http_mock.assert_called_once_with(
+            DEFAULT_WEBHOOK_URL,
+            method='POST',
+            json={'entity_id': 'input_boolean.test', 'new_state': 'off', 'old_state': 'on'},
+            headers={}
+        )
